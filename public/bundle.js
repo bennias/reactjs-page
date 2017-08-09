381ff7ed668db4a34a1c3b50e2cbe790fd39f335
hacktiv <benedikt.asal@gmx.de>
2017-08-09 16:47:47 +0200

    Renamed About Page

--- public/bundle.js
+++ public/bundle.js
@@ -27187,9 +27187,18 @@
 
 	var About = function About(props) {
 	    return React.createElement(
-	        'h3',
+	        'div',
 	        null,
-	        'About Component'
+	        React.createElement(
+	            'h3',
+	            null,
+	            'About'
+	        ),
+	        React.createElement(
+	            'p',
+	            null,
+	            'It\'s Hacktiv!'
+	        )
 	    );
 	};
 	module.exports = About;
